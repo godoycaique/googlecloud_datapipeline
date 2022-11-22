a78b63b33854a83c042eb5321f9f6a398769dc7a
Caique Godoy <57337625+godoycaique@users.noreply.github.com>
2022-11-22 15:30:09 -0300

    update

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "gcs_bucket"{
-    name = "test-bucket-random-6278"
+resource "google_storage_bucket" "gcs_bucket" {
+    name = "test-bucket-random-654654564654"
     location = var.region
 }
